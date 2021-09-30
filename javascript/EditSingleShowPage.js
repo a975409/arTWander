a17a73d355de320c79458c6f246a785a71dacbb0
pangpang <you@example.com>
2021-10-01 02:48:07 +0800

    update feature/MemberPage

--- javascript/EditSingleShowPage.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('resize', windowResize);
-windowResize();
-function windowResize(){
-  let wd = this;
-  let h = wd.innerHeight;
-  let scrollContent = document.getElementById("scrollContent");
-  let height = h - scrollContent.offsetTop;
-  scrollContent.style.height = height + "px";
-}
