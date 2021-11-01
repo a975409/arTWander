ef25e35d4f09a5e0c153041db3ce15f4f0385b6a
a975409 <a975409@gmail.com>
2021-11-02 04:00:14 +0800

    1.編輯展演的功能已完成，可移除已上傳的圖片

--- arTWander/javascript/EditSingleShowPage.js
+++ arTWander/javascript/EditSingleShowPage.js
@@ -27,6 +27,32 @@ $('#imgFiles').MultiFile({
         selected: '$file',
         denied: '檔案指定失敗，原因如下：\n$ext!',
         duplicate: '下列圖檔已重複選取:\n$file!'
+    },
+    afterFileSelect: function (element, value, master_element) {
+
+        if (value.length > 20) {
+            let imgFiles = document.querySelectorAll('.MultiFile-remove+span>.MultiFile-label');
+            let a_remove = document.getElementsByClassName('MultiFile-remove');
+
+            for (let i in imgFiles) {
+                if (imgFiles[i].nodeType == 1) {
+                    //    console.log(imgFiles[i].title);
+
+                    if (imgFiles[i].title == value) {
+                        a_remove[i].click();
+                        break;
+                    }
+                }
+            }
+
+            Swal.fire({
+                icon: 'error',
+                title: '無法指定檔案',
+                text: '檔名+副檔名超過20個字',
+                showConfirmButton: false,
+                showCancelButton: true
+            });
+        }
     }
 });
 
@@ -60,3 +86,63 @@ $('#CreateShowForm').submit(function () {
 
     $('#searchKeyword').val(result);
 });
+
+function getShowImages(showPageId) {
+    $.ajax({
+        type: 'get',
+        url: './getShowImages/ShowPageManage',
+        data: { showPageId: showPageId },
+        success: function (data) {
+            $('#showImgs').html('');
+            $('#showImgs').append(data);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            Swal.fire({
+                icon: 'error',
+                title: textStatus,
+                text: errorThrown,
+                showConfirmButton: false,
+                showCancelButton: true
+            });
+        }
+    })
+}
+
+function removeShowImage(imgId, showPageId) {
+    Swal.fire({
+        title: '移除圖片?',
+        text: "是否要移除這張已上傳的圖片？",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: '是！確定移除',
+        cancelButtonText: '否！不要移除',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                type: 'post',
+                url: './removeShowImage/ShowPageManage',
+                data: { imgId: imgId, showPageId: showPageId},
+                success: function (data) {
+                    Swal.fire(
+                        '移除成功!',
+                        '已移除圖片',
+                        'success'
+                    ).then(function () {
+                        getShowImages(showPageId);
+                    });
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: textStatus,
+                        text: errorThrown,
+                        showConfirmButton: false,
+                        showCancelButton: true
+                    });
+                }
+            });
+        }
+    });
+}
