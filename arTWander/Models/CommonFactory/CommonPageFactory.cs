e7c5d09ea17309309d501730f00575c04d1b9d44
PinHungChen <didi3325s@hotmail.com>
2021-11-08 13:06:37 +0800

    1. 新增展演單位清單與個別葉面 2.新增與移出至我的最愛功能 3.新增展覽個別畫面與新增至最愛功能

---
+++ arTWander/Models/CommonFactory/CommonPageFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace arTWander.Models.CommonFactory
+{
+    public class CommonPageFactory
+    {
+        private ApplicationDbContext _dbContext;
+
+        public CommonPageFactory(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public IQueryable<CommonCompanyViewModel> queryAllCustomer()
+        {
+            var companyList = _dbContext.Company.Select(m => new CommonCompanyViewModel
+            {
+                Id = m.Id,
+                CompanyName = m.CompanyName,
+                CompanyCity = m.City.CityName,
+                PhotoSticker = "/SaveFiles/Company/" + m.Id + "/Info/" + m.PhotoStickerImage,
+            });
+            return companyList;
+        }
+        public IQueryable<CommonCompanyViewModel> queryCustomerByCity(int? CityId)
+        {
+            var companyList = _dbContext.Company.Where(m=>m.FK_City == CityId).Select(m => new CommonCompanyViewModel
+            {
+                Id = m.Id,
+                CompanyName = m.CompanyName,
+                CompanyCity = m.City.CityName,
+                PhotoSticker = "/SaveFiles/Company/" + m.Id + "/Info/" + m.PhotoStickerImage,
+            });
+            return companyList;
+        }
+
+
+    }
+}
